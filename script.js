1251afeb897f594905ab6564c705e2d9ad133725
Irokanade <michaeljs.leong@gmail.com>
2023-05-24 00:53:52 +0800

    feat: volume function added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var volumeSlider = document.getElementById("musicVolume");
+
 function play() {
     var music = document.getElementById("music");
     music.loop = true; // Set the loop property to true
@@ -8,3 +10,10 @@ function pause() {
     var music = document.getElementById("music");
     music.pause();
 }
+
+function adjustVolume(volume) {
+    var volume = volumeSlider.value / 100; // Convert range 1-100 to 0.01-1
+    music.volume = volume;
+}
+
+volumeSlider.addEventListener("input", adjustVolume);
