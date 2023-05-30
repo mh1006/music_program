0af113c62a71fe01e8bfa12cc2cd3f70492d53b0
Irokanade <michaeljs.leong@gmail.com>
2023-05-30 12:40:12 +0800

    fix: sliders fixed

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-var volumeSlider = document.getElementById("musicVolume");
 var lastPlaying = false;
 
 var musicFiles = [
@@ -12,18 +10,15 @@ var musicFiles = [
     'music/路上.mp3'
 ];
 
-function adjustVolume(volume) {
-    var audio = document.getElementById('audioPlayer');
-    var volume = volumeSlider.value / 100; // Convert range 1-100 to 0.01-1
-    audio.volume = volume;
-}
-
-function adjustVolume(audioId, sliderId) {
+function adjustVolume(audioId, sliderId, outputId) {
     var audio = document.getElementById(audioId);
     var slider = document.getElementById(sliderId);
     var volume = slider.value / 100;
     
     audio.volume = volume;
+
+    var output = document.getElementById(outputId);
+    output.innerHTML = slider.value;
   }
 
   function toggleSound(audioId, containerId) {
@@ -63,11 +58,6 @@ function adjustVolume(audioId, sliderId) {
       lastPlaying = false;
     }
   }
-  
-
-volumeSlider.addEventListener("input", adjustVolume);
-
-var audioPlayer = document.getElementById('audioPlayer');
 
 function displayCurrentlyPlaying() {
     var currentlyPlayingElement = document.getElementById('currentlyPlaying');
